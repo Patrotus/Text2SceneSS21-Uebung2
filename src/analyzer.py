7ba50eb2b4f256cd6b738e470a1d8f2f88c8c034
Patrotus <ghbv959@gmail.com>
2021-04-28 13:13:49 +0200

    Updated comments

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,92 +1,131 @@
-from dict_functions import update_dict_occurences, update_dict_amount
+import os
 import matplotlib.pyplot as plt
+from dict_functions import update_dict_occurences, update_dict_amount, sort_dict, output_csv, print_dict_or_tuple_list
+from progress_bar import progress
 
 
 class Analyzer:
-    sentences = {}
-    total_pos = {}
-    total_tag = {}
-    qs_type_dict = {}
-    verbs = {}
-    link_pre = {}
+    """
+    The Analyuer object recieves a list of FileAnalyzer and performs an overall analysis on them and outputs the
+    results to some .csv files
+
+    :param fas: List of FileAnalyzer
+    """
+    # Sets variables
+    __sentences = {}
+    __total_pos = {}
+    __total_tag = {}
+    __qs_type_dict = {}
+    __verbs = {}
+    __link_pre = {
+        'QSLINK': {},
+        'OLINK': {}
+    }
 
     def __init__(self, fas):
-        self.fas = fas
-        self.analyze()
+        self.__fas = fas
+        self.__analyze()
 
-        # Brings the sentences and verbs in the required format
-        sent_items = list(self.sentences.items())
-        sent_items.sort(key=lambda tup: tup[0])
-        self.sentences_array = sent_items
-        verbs = list(self.verbs.items())
-        verbs.sort(key=lambda tup: tup[1], reverse=True)
-        self.verbs_array = verbs[:5]
+        # Brings the sentences and __verbs in the required format
+        self.__sentences_array = sort_dict(self.__sentences, True)
+        self.__verbs_array = sort_dict(self.__verbs, True)[:5]
 
-    def debug(self):
-        print(self.total_tag)
-        print(self.total_pos)
-        print(self.verbs_array)
-        print(self.qs_type_dict)
-        print(self.link_pre)
+    def __analyze(self):
+        """
+        Runs an Analysis over all FileAnalyzers in the Analyzer
+        """
+        print('[INFO] Starting Analysis')
 
-    def analyze(self):
-        for fa in self.fas:
+        prog = 0
+        total_length = len(self.__fas)
+
+        for fa in self.__fas:
+            # Updates Total PoS
             fa_pos_counter = fa.get_pos_counter()
-            self.update_total_pos(fa_pos_counter)
+            [update_dict_amount(self.__total_pos, key, fa_pos_counter[key]) for key in fa_pos_counter]
 
+            # Updates everything tag related
             fa_tags = fa.get_tags()
-            self.update_tag_dicts(fa_tags)
+            self.__update_all_tag_dicts(fa_tags)
 
-            fa_sentences = fa.get_sentences()
-            self.update_sentence_lengths(fa_sentences)
+            # Updates the sentences distribution
+            [update_dict_occurences(self.__sentences, sent) for sent in fa.get_sentences()]
 
-    def update_tag_dicts(self, fa_tags):
+            # Prints progressbar
+            prog += 1
+            progress(prog, total_length, 'Analysing')
+        print('\n[INFO] Analysis done')
+
+    def __update_all_tag_dicts(self, fa_tags):
         """
-        Performs all neccesary updates for the tags of a documents
-        :param fa_tags:
+        Performs all necessary updates for the tags of a documents
+        :param fa_tags: List of all tags of an FileAnalyzer
         """
-        for item in fa_tags:
-            tag = item.tag
+        # Gets all SPATIAL_SIGNAL from the tags
+        spatial_signal_tags = {item.attrib['id']: item.attrib['text'] for item in fa_tags.findall("SPATIAL_SIGNAL")}
+
+        for tag in fa_tags:
+            tag = tag.tag
+
             # Updates QSLINK type distribution
             if tag == 'QSLINK':
-                update_dict_occurences(self.qs_type_dict, item.attrib['relType'])
+                update_dict_occurences(self.__qs_type_dict, tag.attrib['relType'])
+                self.__update_trigger_dict(tag, spatial_signal_tags, 'QSLINK')
+
             # Updates the preposition distribution for QS and OLINK
-            if tag == 'QSLINK' or tag == 'OLINK':
-                if item.attrib['trigger']:
-                    tag_list = fa_tags.findall(f"./SPATIAL_SIGNAL[@id='{item.attrib['trigger']}']")
-                    if len(tag_list) > 0:
-                        pre = tag_list[0].attrib['text']
-                        update_dict_occurences(self.link_pre, pre)
-            # Updates the distribution for motion verbs
+            elif tag == 'OLINK':
+                self.__update_trigger_dict(tag, spatial_signal_tags, 'OLINK')
+
+            # Updates the distribution for motion __verbs
             elif tag == 'MOTION':
-                update_dict_occurences(self.verbs, item.attrib['text'])
-            update_dict_occurences(self.total_tag, tag)
+                update_dict_occurences(self.__verbs, tag.attrib['text'])
 
-    def update_sentence_lengths(self, sentences):
-        """
-        Updates the distribution of sentence lenghts
-        :param sentences:
-        """
-        for sent in sentences:
-            update_dict_occurences(self.sentences, sent)
+            # Updates total Tag-Count
+            update_dict_occurences(self.__total_tag, tag)
 
-    def update_total_pos(self, fa_pos_counter):
+    def __update_trigger_dict(self, tag, signal_dict, key):
         """
-        Updates the distribution of the pos tags
-        :param fa_pos_counter:
+        Updates the dictionary keeping the amounts of triggers for QS-/OLINK
+        :param tag: Tag
+        :param signal_dict: Dictionary of spatial Signals
+        :param key: Defines which dictionary in link_pre should be updates
         """
-        for key in fa_pos_counter:
-            update_dict_amount(self.total_pos, key, fa_pos_counter[key])
+        if 'trigger' in tag.attrib:
+            trigger = tag.attrib['trigger']
+            if trigger in signal_dict:
+                update_dict_occurences(self.__link_pre[key], signal_dict[trigger])
 
     def plot_sentence_distribution(self):
         """
         Plots the distribution of the sentences lengths
         """
         x_val, y_val = [], []
-        for x in self.sentences_array:
+        for x in self.__sentences_array:
             x_val.append(x[0])
             y_val.append(x[1])
         plt.scatter(x_val, y_val, s=2, c="black", marker='o')
         plt.ylabel('Occurrences')
         plt.xlabel('Sentence Length')
         plt.show()
+
+    def output_results_files(self):
+        """
+        Puts all results of the analysis in a .csv
+        """
+        file_path = os.path.join('..', 'results', 'csv')
+        output_csv(file_path, 'total_pos', sort_dict(self.__total_pos, True), ['PoS', 'Amount'])
+        output_csv(file_path, '__total_tag', sort_dict(self.__total_tag, True), ['Tag', 'Amount'])
+        output_csv(file_path, 'total_qs_type', sort_dict(self.__qs_type_dict, True), ['QSLINK-relType', 'Amount'])
+        output_csv(file_path, 'top_5_verbs', self.__verbs_array, ['Verb', 'Amount'])
+        [output_csv(file_path, f'total_link_{key}', sort_dict(self.__link_pre[key], True), ['Trigger', 'Amount'])
+         for key in self.__link_pre]
+
+    def debug(self):
+        """
+        Prints out all the results of the analysis
+        """
+        print_dict_or_tuple_list(self.__total_pos, 'Verteilung der PoS')
+        print_dict_or_tuple_list(self.__total_tag, 'Verteilung der Tags')
+        print_dict_or_tuple_list(self.__qs_type_dict, 'Verteilung der "relTypen" in QSLINK')
+        print_dict_or_tuple_list(self.__verbs_array, '5 häufigste MOTION-Verben')
+        [print_dict_or_tuple_list(self.__link_pre[tag_type], f'Verteilung von {tag_type}') for tag_type in self.__link_pre]
