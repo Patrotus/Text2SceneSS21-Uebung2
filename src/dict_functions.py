18a578e00b7dfb861bee3d462fde83cd6eb6dc98
Patrotus <ghbv959@gmail.com>
2021-04-28 12:58:37 +0200

    Updated comments + added Results

--- src/dict_functions.py
+++ src/dict_functions.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 def update_dict_occurences(up_dict, key):
     """
     Deals with key and counts up its occurences
@@ -33,7 +37,7 @@ def sort_dict(dictionary, descending=False):
     return sorted(dictionary.items(), key=lambda x: x[1], reverse=descending)
 
 
-def sort_list(unsorted_list, descending=False):
+def sort_tuple_list(unsorted_list, descending=False):
     """
     Sorts a tuple list using the second element of
     :param unsorted_list: List to be sorted
@@ -41,3 +45,41 @@ def sort_list(unsorted_list, descending=False):
     :return: list: List of Tuples
     """
     return sorted(unsorted_list, key=lambda x: x[1], reverse=descending)
+
+
+def print_dict_or_tuple_list(print_object, text=''):
+    """
+    Prints out a Dictionary or a list of tuples
+    :param print_object: Dictionary or List of Tuples
+    :param text: Heading to be printed above
+    """
+    if text != '':
+        print(f'=== {text} ===')
+
+    if isinstance(print_object, list):
+        sorted_object = sort_tuple_list(print_object, True)
+    else:
+        sorted_object = sort_dict(print_object, True)
+    [print(f'{item[0]} - {item[1]}') for item in sorted_object]
+
+
+def output_csv(path, name, output_list, heading):
+    """
+    Outputs a list to a .csv file
+    :param path: Path where the file should be saved
+    :param name: Name of the file
+    :param output_list: list of tuples which should be added to file
+    :param heading: Header of the .csv file
+    """
+    # Creates folder if it doesn´t exist
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    file_path = os.path.join(path, f'{name}.csv')
+
+    # Writes row to .csv file
+    with open(file_path, 'w', newline='') as new_file:
+        writer = csv.writer(new_file, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(heading)
+        for entry in output_list:
+            writer.writerow(entry)
